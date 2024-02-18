556dbe8868898bd926ef99d76abee475fdf382a6
NikitaSemenukha <skarabaychick@gmail.com>
2024-02-19 00:35:13 +0100

    fix: update

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import Navbar from "../Navbar/Navbar.jsx";
+import LeftSidebar from "../Sidebars/LeftSidebar";
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
   return (
-    <div>
-      <Navbar></Navbar>
+    <div className="w-full">
+      <div className="fixed top-0 z-10 w-full bg-white">
+        <Navbar></Navbar>
+      </div>
+      <div className="flex bg-gray-100">
+        <div className="flex-auto w-[20%] fixed top-12">
+          <LeftSidebar></LeftSidebar>
+        </div>
+      </div>
     </div>
   );
 };
