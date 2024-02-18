940b9dbc3bd75980cb847ad65515782f36e66f64
NikitaSemenukha <skarabaychick@gmail.com>
2024-02-18 22:17:24 +0100

    feat: add some pages, and Navbar

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar.jsx";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Home;
